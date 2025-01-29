9838a1bba7343551ef748d04a3229c72ffb2b398
Mateus Sanchez <mateo705@hotmail.com>
2025-01-29 17:17:51 -0500

    new:Inicializacion de proyecto.

---
+++ Backend/Facturacion/src/test/java/facturacion/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package facturacion.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
